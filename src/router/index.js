8a7b8d4458f360b4c7bbf70c6e63d5c39d201ea0
PaulaDanthu <danthupaula@gmail.com>
2023-03-14 14:18:50 +0100

    router modif

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import HomePage from '../views/HomePage.vue'
 
 
 const routes = [
-    {path: '/', name: 'HomePage', component: HomePage}
+    {path: '/my-visit-card/', name: 'HomePage', component: HomePage}
 ]
 
 const router = createRouter({
