1178d14b06b8481ac2370eb0c5bbcdd8736363ea
PaulaDanthu <danthupaula@gmail.com>
2023-03-14 13:52:12 +0100

    undo the modif

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,8 @@ const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "xmlhttprequest-ssl": "./node_modules/engine.io-client/lib/xmlhttprequest.js"
-    }
+  alias:{
+    '@' : path.resolve(__dirname, '.src')
   },
   plugins: [vue()],
 })
